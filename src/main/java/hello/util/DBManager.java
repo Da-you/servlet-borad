dce7a183ac55a9d0b9cbae7f1f9f9db58499056c
JoHyunwoo <jhw3874@gmail.com>
2024-08-02 17:19:25 +0900

    typo

--- src/main/java/hello/util/DBManager.java
+++ src/main/java/hello/util/DBManager.java
@@ -1,23 +1,22 @@
 package hello.util;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 // DB Connection 객체를 얻고 해제를 담당하는 클래스
 public class DBManager {
-    public static Connection getConnection() {
-        Connection conn = null;
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null; //
         try {
-            Context context = new InitialContext(); // ?
-            Context envContext = (Context) context.lookup("java:/comp/env"); // Todo :
-            DataSource ds = (DataSource) envContext.lookup("jdbc/mysql"); // Todo :
-            conn = ds.getConnection();
+//            Context context = new InitialContext(); // ?
+//            Context envContext = (Context) context.lookup("java:/comp/env"); // Todo :
+//            DataSource ds = (DataSource) envContext.lookup("jdbc/mysql"); // Todo :
+//            conn = ds.getConnection();
+            Class.forName("com.mysql.cj.jdbc.Driver"); // jdbc 드라이버 로드
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/server","root","kfcayo123"); // Todo :  실제로 db와 연결되어 작업을 수행할 수 있는 통로로 작용하는 객체 변수로 해당 객체가 생성되면 db 접근이 가능해진다.
+            System.out.println("DB connection success");
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("DB connection failed");
         }
         return conn;
     }
@@ -32,7 +31,7 @@ public class DBManager {
         }
     }
 
-    // DML(insert, update, delete) 수행ㅎ루 리소스 해제
+    // DML(insert, update, delete) 수행후 리소스 해제
     public static void close(Connection conn, Statement stmt) {
         try {
             stmt.close();
